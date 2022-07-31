5f0bfe5d64c0ab6852fcfcb8fba8c6c881c81064
hitorigoya <75735750+hitorigoya@users.noreply.github.com>
2022-07-31 23:51:31 +0900

    目次機能の追加

---
+++ assets/js/toc.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.single_article__')
+    const firstH2 = container.querySelector('h2')
+
+    if (!firstH2) {
+        return
+    }
+
+    const toc = document.createElement('ul')
+    toc.classList.add('single_toc__')
+
+    const tocTitle = document.createElement('div')
+    tocTitle.textContent = '目次'
+    tocTitle.classList.add('single_toc_title__')
+    toc.append(tocTitle)
+
+    const headings = container.querySelectorAll('h2')
+    headings.forEach((heading, index) => {
+        const anchor = document.createElement('a')
+        anchor.textContent = heading.textContent
+
+        if (heading.id) {
+            anchor.href = '#' + heading.id
+        } else {
+            heading.id = 'toc' + index
+            anchor.href = '#toc' + index
+        }
+
+        const listItem = document.createElement('li')
+        listItem.append(anchor)
+        toc.append(listItem)
+    })
+
+    firstH2.insertAdjacentElement('beforebegin', toc)
+})
